db562df3a861d688672bb960efa150f05cd32f22
Douglas Mendes <douglasleomendes@hotmail.com>
2026-02-17 10:25:30 +0100

    Bootstrap engine repo: CMake + engine/game skeleton

---
+++ engine/src/main_engine_dummy.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+namespace Canary {
+	void EngineHello() {
+		std::cout << "CanaryEngine: Hello!\n";
+	}
+}
---
+++ game/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace Canary { void EngineHello(); }
+
+int main() {
+	std::cout << "Game boot ... \n";
+	Canary::EngineHello();
+	return 0;
+}
